b483d58c41bced328d58516023babcb155c90765
DELL <DELL@DESKTOP-G6KR5CF>
2022-02-14 17:30:59 +1000

    Commit Task

--- Sorting Algorithm/src/Sortting_Algorithm_Code/Names.java
+++ Sorting Algorithm/src/Sortting_Algorithm_Code/Names.java
@@ -4,21 +4,17 @@ public class Names {
 
 	private String firstNamePart;
 	private String surName;
-
 	public Names(String firstNamePart, String surName)
 	{
 	    this.firstNamePart=firstNamePart;
-	    this.surName=surName;
-	    
+	    this.surName=surName;   
 	}
 	public String getSurName()
-	{
-		
+	{	
 	    return surName;
 	}
 	public String getFirstNamePart()
 	{
 	    return firstNamePart;
 	}
-
 }
